24062fcc069694a3dfad2ed5017ee496e7a1a1b6
Nicola Buckhurst <nicola@buckhurst.org>
2013-03-05 14:25:08 +0000

    Finish Sign In

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -56,7 +56,7 @@ describe "Static pages" do
     click_link "Home"
     click_link "Sign up now!"
     page.should have_selector 'title', text: full_title('Sign up')
-    click_link "Sample app"
+    click_link "sample app"
     page.should have_selector 'title', text: full_title('')
   end
 
